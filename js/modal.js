9a757a77a174e5bc6df607c3c666d98e145ae2bf
Ihor Saievych <55617503+ihorsaievych@users.noreply.github.com>
2022-06-22 11:18:14 +0300

    fixed.width size - no scroll

--- js/modal.js
+++ js/modal.js
@@ -4,10 +4,24 @@
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
     body: document.querySelector("body"),
+    // header: document.querySelector('[data-page-header]'),
+    // forms: document.querySelectorAll('.js-speaker-form'),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   // padding for fixed header
+//  if (refs.header.classList.contains('page-header--fixed')) {
+//   const { height: headerHeight } = refs.header.getBoundingClientRect();
+//   document.body.style.paddingTop = `${headerHeight}px`;
+//   }
+//   // forms submit handler
+//   refs.forms.forEach(item =>
+//   item.addEventListener('submit', e => {
+//     e.preventDefault();
+//     new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
+//   }))
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
